827f3a845654cef31104e5fcb58375e54fa33635
Anthony Gabriel de Jesus <anthony04jesus@gmail.com>
2025-07-07 19:27:57 -0300

    Update tile.cpp

--- testes/Version_1/code/tile.cpp
+++ testes/Version_1/code/tile.cpp
@@ -1,29 +1,7 @@
 #include "tile.h"
 
-
-
-
-//Tiles(const QString &text , QWidget *parent = nullptr);
-
 Tile::Tile(QWidget * parent): QPushButton(parent)
 {
     this->bomb = false;
     this->state = Unopened;
 }
-
-// void Tile::addAdjacente(Tile *t)
-// {
-//     adjacentes.push_back(t);
-// }
-
-
-// void Tile::keyPressEvent(QKeyEvent *event)
-// {
-//     int k = event->key();
-//     //if(k == Qt::Key_Space)
-//     //_restart->animateClick(100);
-// }
-// void Tile::keyPressed()
-// {
-
-// }
